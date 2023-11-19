4681686c5425bceb8633cb188290c95eb239de3a
Chris Liloba <110470679+Chris-Liloba@users.noreply.github.com>
2023-11-19 16:47:52 +0300

    Update ManipulationOfListByUserCode.py

--- ManipulationOfListByUserCode.py
+++ ManipulationOfListByUserCode.py
@@ -19,8 +19,6 @@ totalMarks = kiswahili + maths + english + history + GHCRE
 print(f"+++++++-----TOTAL MARKS+++++++-----:\n {totalMarks}")
 
 
-for list_1 in myList:
-    list_1 += list_1
-    print(f" list_1: {list_1}")
+
 MeanGrade = (kiswahili + maths + english + history + GHCRE)/5
 print(f"+++++++-----MeanGrade+++++++-----:\n {MeanGrade}")
